a0f462ba80528bec7148d4926bb7250b74eea8ed
ArchitGharat <74640210+ArchitGharat@users.noreply.github.com>
2020-11-21 06:50:28 -0800

    Class and attributes

---
+++ Class1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+Class Greetings
+{
+   public:
+        void Have a Great day()
+        {
+             cout<<" Have a great day"<<endl;
+        }
+    };
+    
+    int main()
+    {
+
+     Greetings h:
+     h.Have a Great day();
+     
+     return 0;
+}
