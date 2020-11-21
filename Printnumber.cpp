6ba6b50a689263c25f3418dc854126634b6f4f02
ArchitGharat <74640210+ArchitGharat@users.noreply.github.com>
2020-11-21 06:18:09 -0800

    Print numer.cpp

---
+++ Printnumber.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<conio.h>
+using namespace std ;
+int main()
+{
+ int a;
+ cout<<"Enter a number:";
+ cin>>a;
+ cout<<" The number entered by user is :"<<a;
+ getch();
+}
+ 
