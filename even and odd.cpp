35eb92f391ce6bda24eacc3f7933fff7c224e34b
ArchitGharat <74640210+ArchitGharat@users.noreply.github.com>
2020-11-21 06:25:05 -0800

    Even and Odd

---
+++ even and odd.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std ;
+int main()
+{
+ int n;
+ cout<<"Enter the number:";
+ cin>>n;
+ 
+ if(n % 2 ==0)
+     cout<<" The number entered by user is an even :";
+ else
+
+     cout<<" The number entered by user is an odd :";
+ return0;
+}
