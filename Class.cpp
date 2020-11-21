575a8c7592d9ad4cd06383991c5d9b0118a4073d
ArchitGharat <74640210+ArchitGharat@users.noreply.github.com>
2020-11-21 06:44:25 -0800

    Class2

---
+++ Class.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class student(){
+public:
+    int roll no;
+   string.Name ;
+   string.Branch;
+   ;
+   int main()
+   {
+        student std;
+    
+        std.rollno= 12;
+        std.Name = "Archit Gharat";
+        std.Branch = "EXTC";
+    
+    cout<<"Student's Roll no :"<< std.rollno <<"\n";
+    cout<<"Student's Name :"<< std.rollno <<"\n";
+    cout<<"Student's Branch:"<< std.rollno <<"\n";
+    return 0;
+}
