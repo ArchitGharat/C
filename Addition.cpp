d75efa67aafa57a44a2036d8f488227bc288ed4e
ArchitGharat <74640210+ArchitGharat@users.noreply.github.com>
2020-11-21 06:21:10 -0800

    Addition

---
+++ Addition.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<conio.h>
+using namespace std ;
+int main()
+{
+ int a,b,c;
+ cout<<"Enter two numbers:";
+ cin>>a>>b;
+ cout<<" The sum of two numbers is :"<<c;
+ getch();
+}
